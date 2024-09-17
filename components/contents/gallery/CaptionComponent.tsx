f36bf2b7f61829286183f1c711769597e7d938a4
SamHalera <samuel.halera@gmail.com>
2024-09-17 22:07:53 +0200

    content and style for photos and vide pages + lightgallery for photos

---
+++ components/contents/gallery/CaptionComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CaptionComponent = () => {
+  return <div>CAPTION</div>;
+};
+
+export default CaptionComponent;
---
+++ components/contents/mediaFrames/TContentMediaFrames.d.ts
@@ -0,0 +1,10 @@
+type TContentMediaFrames = {
+  title: string;
+  Media: TMediaComponent[];
+};
+
+type TMediaComponent = {
+  id: number;
+  title: string;
+  iframe: string;
+};
